92e8f0a17cfa0bc307dcb3af098687bb3abe48a7
Alex Telon <Alex Telon>
2023-09-04 09:54:35 +0200

    Added first solution and improved tests

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from itertools import cycle
+
 
 def solve(n):
-    print('#')
+    # Generator for a pattern of smaller and smaller squares of # and o alternating.
+    pattern = cycle('#.')
+    squares = [[[next(pattern)] * side] * side for side in range(n, 0, -2)]
+
+    # Drawing the smaller squares inside the bigger ones on top of eachother.
+    final_grid = [['x' for _ in range(n)] for y in range(n)]
+    for offset, square in enumerate(squares):
+        for y, row in enumerate(square):
+            for x, c in enumerate(row):
+                final_grid[y+offset][x+offset] = c
+
+    for row in final_grid:
+        print("".join(row))
+
+
+if __name__ == "__main__":
+    solve(12)
--- tests/test_samples.py
+++ tests/test_samples.py
@@ -2,17 +2,39 @@ from main import solve
 from contextlib import redirect_stdout
 import io
 
+def utility(n):
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        solve(n)
+    return buffer.getvalue().strip()
 
 def test_1():
-    buffer = io.StringIO()
-    with redirect_stdout(buffer):
-        solve(1)
-    out = buffer.getvalue().strip()
-    assert out == '#'
+    assert utility(1) == '#'
 
 def test_2():
-    buffer = io.StringIO()
-    with redirect_stdout(buffer):
-        solve(2)
-    out = buffer.getvalue().strip()
-    assert out == '##\n##'
+    assert utility(2) == '##\n##'
+
+def test_9():
+    ans = """#########
+#.......#
+#.#####.#
+#.#...#.#
+#.#.#.#.#
+#.#...#.#
+#.#####.#
+#.......#
+#########"""
+    assert utility(9) == ans
+
+def test_10():
+    ans = """##########
+#........#
+#.######.#
+#.#....#.#
+#.#.##.#.#
+#.#.##.#.#
+#.#....#.#
+#.######.#
+#........#
+##########"""
+    assert utility(10) == ans
